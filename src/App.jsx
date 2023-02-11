13ad4c1b3c39d958c2a7d82f9c054c7bdca85400
Maximilian <scientificresearch309@gmail.com>
2023-02-11 01:52:52 +0100

    Both the counter and input are working well!

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+const Password = 123;
 function App() {
   const [count, setCount] = useState(0);
+  const [theInput, setTheInput] = useState("");
+  const [userIsOnline, setUserIsOnline] = useState(false);
+  const handleChange = (e) => {
+    // e.preventDefault();
+    setTheInput(e.target.value);
+  };
 
   const handleButton = () => {
-    // const counter = counter + 1;
-    // count = count + 1;
-    setCount(count + 1);
+    {
+      Password === Number(theInput) &&
+        (setCount(count + 1),
+        setUserIsOnline(true));
+    }
   };
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <button onClick={handleButton}>{count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Press the button to start to count:</p>
+      <input type="text" value={theInput} onChange={(e) => handleChange(e)} />
+      {/* {theInput} */}
+      <button onClick={handleButton}>{count}</button>
+      {userIsOnline && <p>Password is correct, You are Online!</p>}
     </div>
   );
 }
